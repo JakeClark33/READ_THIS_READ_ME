710875866280c2a7726b2d7e67479a74d3363221
JakeClark33 <84428608+JakeClark33@users.noreply.github.com>
2021-07-25 12:47:22 -0500

    working template

--- Develop/index.js
+++ Develop/index.js
@@ -1,30 +1,32 @@
-const fs = require('fs');
-const pageDataArgs = process.argv.slice(2, process.argv.length);
-const [projectType, projectName] = pageDataArgs;
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
+const inquirer = require('inquirer');
 
-    <body>
-    <h1>${projectType}</h1>
-    <h2>${projectName}</h2>
-  </body>
-  </html>
-  `;
-};
-fs.writeFile('index.html', generatePage(projectType, projectName), err => {
-    if (err) throw err;
+inquirer
+    .prompt ([
+        {
+            type: 'input',
+            name: 'Project type',
+            message: 'What type of document and you creating?'
+        },
+        {
+            type: 'input',
+            name: 'Project Name',
+            message: 'Please enter the name of your project(Required)'
+        }
+    ])
+    .then(answers => console.log(answers));
+
+//  const fs = require('fs');
+
+//  const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(projectType, projectName);
+
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
   
-    console.log('README complete! Check out index.html to see the output!');
-  });
-
+//     console.log('README complete! Check out index.html to see the output!');
+//   });
 
 
 
